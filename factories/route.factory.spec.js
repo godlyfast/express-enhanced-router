5df10142bbacd3799b70e1f5b0a19c1c81bf1558
Tim Smyr <godlyfast@gmail.com>
2020-01-26 18:17:59 +0200

    tests route facrory

--- factories/route.factory.spec.js
+++ factories/route.factory.spec.js
@@ -1,11 +1,37 @@
 const { RouteFactory } = require('./route.factory');
-const ServiceContainer = require('../helpers/service-container.helper')
+const ServiceContainer = require('../helpers/service-container.helper');
+class ResponseMock {
+    constructor(cb) {
+        this.response = {
+            status: null,
+            data: null
+        };
+        this.cb = cb;
+    }
+    status(status) {
+        this.response.status = status;
+        return this;
+    }
+    json(...args) {
+        this.response.data = args[0];
+        this.cb(this.response);
+        return this;
+    }
+}
+
 describe('Route Factory', () => {
     it('Should work', () => {
         ServiceContainer.registerProvider("FooController", () => ({
-            getFoo: () => {}
+            getFoo: () => Promise.resolve({foo: true})
         }));
         const r = RouteFactory([{name: 'foo', action: 'getFoo', controller: 'FooController'}]);
         expect(r).toBeTruthy();
+        expect(r.foo).toBeDefined();
+        const responseMock = new ResponseMock((response) => {
+            expect(response.status).toBe(200);
+            expect(response.data).toHaveProperty('foo');
+            expect(response.data.foo).toBe(true);
+        });
+        r.foo({}, responseMock);
     });
 });
